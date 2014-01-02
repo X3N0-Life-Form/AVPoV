143b9069c13fc0977b198ff90c14c1539cecf0fa
X3N0-Life-Form <adrarsis@laposte.net>
2014-01-02 20:10:47 +0100

    added the (non-functional) turret swap scrip to the repo

---
+++ data/scripts/turretSwap.lua
@@ -0,0 +1,20 @@
+for index=0, #mn.Ships do
+	currentShip = #mn.Ships[index]
+	-- bombers
+	if currentShip.Class.Name == "SB Seraphim"
+		or currentShip.Class.Name == "SB Nephilim" then
+--
+Graphics.setColor(255, 255, 255)
+Graphics.drawString("Hello, world!", 5, 10)
+--
+		mainBankClass = currentShip.PrimaryBanks[0].WeaponClass
+		for subIndex=0, #currentShip do
+			if (currentShip[subIndex].isTurret()) then
+				currentShip[subIndex].PrimaryBanks[0].WeaponClass = mainBankClass
+			end
+		end
+
+	end
+	-- gunships
+	---- need to decide on weapon packages first
+end
