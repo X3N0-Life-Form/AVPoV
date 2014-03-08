56404b3c48ba58cb0a50430368d12f1993b97645
X3N0-Life-Form <adrarsis@laposte.net>
2014-03-08 17:17:21 +0100

    turretSwap is WORKING

--- data/scripts/turretSwap.lua
+++ data/scripts/turretSwap.lua
@@ -1,16 +1,14 @@
 for index=0, #mn.Ships do
 	currentShip = mn.Ships[index]
+	
 	-- bombers
 	if currentShip.Class.Name == "SB Seraphim"
 		or currentShip.Class.Name == "SB Nephilim" then
---
-Graphics.setColor(255, 255, 255)
-Graphics.drawString("Hello, world!", 5, 10)
---
-		mainBankClass = currentShip.PrimaryBanks[0].WeaponClass
+		
+		mainBankClass = currentShip.PrimaryBanks[1].WeaponClass
 		for subIndex=0, #currentShip do
 			if (currentShip[subIndex]:isTurret()) then
-				currentShip[subIndex].PrimaryBanks[0].WeaponClass = mainBankClass
+				currentShip[subIndex].PrimaryBanks[1].WeaponClass = mainBankClass
 			end
 		end
 
