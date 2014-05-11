78c07826ba7a742f98c0d5d866fa5c77a83d76ab
X3N0-Life-Form <adrarsis@laposte.net>
2014-05-11 21:49:47 +0200

    custom hud gauges - go

---
+++ data/scripts/shivanHUD.lua
@@ -0,0 +1,15 @@
+function activateShivanHUD()
+	mn.evaluateSEXP([[( when
+	( true )
+	( hud-set-custom-gauge-active 
+      ( true ) 
+      "ShivTaskStatusTop" 
+      "ShivTaskStatusBody1" 
+      "ShivTaskStatusBody2" 
+      "ShivTaskStatusBody3" 
+      "ShivTaskStatusBottom" 
+   )
+)]])
+
+
+end
