ea3a4cabd28d65cf4bf1af149d5a0ecfa6136e4a
X3N0-Life-Form <adrarsis@laposte.net>
2015-01-20 14:06:07 +0100

    automated SSM framework: WiP 	custom table file 	data structures 	started writing core functions 	added autoSSM.lua to script init

---
+++ data/scripts/autoSSM.lua
@@ -0,0 +1,91 @@
+--[[
+
+table file syntax:
+
+]]--
+
+
+-----------------------
+--- Global Variable ---
+-----------------------
+tableFile = "data/script-data/auto_ssm.tbl"
+
+strike_info_id = {}	-- [index] = name; contains the name of our automated strikes
+
+--- strike info arrays ---
+--> indexed by auto strike name (see strike_info_id above)
+--> contains data parsed from the table
+
+strike_info_type = {}		-- SSM name, as defined in ssm.tbl
+strike_info_cooldown = {}	-- cooldown, in seconds
+strike_info_team = {}		-- team name
+
+
+--- active strike info ---
+--> indexed by name
+--> contains data related to active automated strikes
+strike_active = {}		-- boolean, is that automated strike type active? Defaults to false
+strike_last_fired = {}		-- mission time at which this strike was last fired
+strike_current_target = {}	-- ship name, current target
+strike_target_list = {}		-- [name][target] = boolean; list of potential targets, used by target-seeking algorithms
+strike_target_type = {}		-- [name][target] = ship/target type; may be used by target-seeking algorithms ??? maybe move this to info ???
+strike_seeker_algo = {}		-- what target algorithm should be used?
+
+
+----------------------------
+--- functions - settings ---
+----------------------------
+
+function auto_ssm_setActive(name, bool)
+	strike_active[name] = bool
+end
+
+function auto_ssm_setTarget(name, target)
+	strike_current_target[name] = target
+end
+
+function auto_ssm_addTarget(name, target)
+	strike_target_list[name][target] = true
+end
+
+function auto_ssm_removeTarget(name, target)
+--TODO
+end
+
+--------------------------
+--- functions - strike ---
+--------------------------
+
+--- cycles through the automated strikes and fires them if necessary
+--- the responsability of calling it is left to the FREDer
+function auto_ssm_cycle()
+	for index, name in pairs(strike_info_id) do
+		if strike_active[name] then
+			cd = strike_info_cooldown[name]
+			lastFired = strike_last_fired[name]
+			if (lastFired + cd >= mn.getMissionTime()) then
+				if (auto_ssm_fire(name)) then
+					strike_last_fired[name] = mn.getMissionTime()
+				end
+			end
+		end
+	end
+end
+
+--- fires the given strike
+--- returns true if fired successfully
+--- Note: the strike's target should be its current target, as determined by its seeking algorithm
+function auto_ssm_fire(name)
+	target = strike_current_target[name]
+
+end
+
+--- updates the given strike's current target, according to its seeking algorithm
+function auto_ssm_updateTarget(name)
+
+end
+
+------------
+--- main ---
+------------
+
