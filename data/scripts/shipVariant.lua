ab3cbc6f89089887335a8c0a36b4fad8a0a083e6
X3N0-Life-Form <adrarsis@laposte.net>
2015-05-08 23:04:49 +0200

    updating the ship variant script to handle the new parsed table format  + updated the sample description  + remove some obsolete stuff

--- data/scripts/shipVariant.lua
+++ data/scripts/shipVariant.lua
@@ -3,22 +3,43 @@
 		-- ship variant manager script --
 		---------------------------------
 In order to work, this script needs:
-	- a descriptor text file
-	- the global variable variantFileName needs to point to this file, eg. "data/config/ship_variants.txt"
-	- call the setVariant() or sv() function via scrip-eval, with the name of the ship you want to alter and the name of the variant of the appropriate class as the function's argument.
-		For instance "sv(Colly, bunny)" means you want Colly to be of the "bunny" variant.
-		Keep in mind that, as of this writing, script-eval suffers from FSO's infamous 32 character limit, so you may want to keep your variant names short for the time being.
-	- alternatively, you can set the lua variable g_sn (global ship name) and g_vn (global variant name) and call the setVariant_g() function:
-		( script-eval
-			"g_sn = Colly"
-			"g_vn = bunny"
-			"setVariant_g()"
-		)
+	- a descriptor table file in data/config
+	- a lua table parser
+	- the global variable variantFileName needs to point to this file, eg. "data/config/ship_variants.tbl"
+	- call the setVariant() function via scrip-eval, with the name of the ship you want to alter and the name of the variant of the appropriate class as the function's argument.
+		For instance "setVariant(Colly, bunny)" means you want Colly to be of the "bunny" variant.
 		
-Descriptor file logic:
-	- A new variant is identified by a '$', followed by the ship class, a ':' and then the variant's name, like this:
-		$GTC Aeolus:	nerfed		--> defines the "nerf" variant for the GTC Aeolus
-	- Following the variant definition is a list of attributes or turret names, potentially with their own sub-attributes.
+Descriptor table logic:
+	- Variants are classified by ship category, which starts with '#'
+	- Each variant starts with '$Name:', follow by attributes that start with "$"
+	- These may be followed by sub attributes, who themselves start with "+"
+	
+Descriptor table sample:
+	#SC Rakshasa
+	
+	$Name:	Lance Assault Cruiser
+	$ai class: Big
+	$subsystem armor: Hell Armor
+	$hull armor:	 Shivan Destroyer Armor
+	$hull:		 80k	;; hull values can be defined using orders of magnitude, eg. 80k = 80,000; 80M = 80,000,000 & 80G = 80,000,000
+	$turret01:Shivan Multi Turret Up
+		+Rof:200
+	$turret02:Shivan Huge Lance
+	$turret03:Shivan Huge Lance
+	$turret04:Shivan Multi Turret Up
+		+Rof:200
+	$turret05:Shivan Multi Turret Up
+	$turret06:Shivan Multi Turret Up
+	$turret07:Shivan Lance
+	$turret08:Shivan Lance
+	$turret09:Shivan Lance
+	$turret10:Shivan Lance
+	$turret11:Shivan Multi Turret Up
+	$turret12:Shivan Multi Turret Up
+	$turret13:Shivan Multi Turret Up
+	$turret14:Shivan Multi Turret Up
+	
+	#End
 		
 			
 Valid attributes:
@@ -52,16 +73,10 @@ Sample entry:
 -- global variables --
 ----------------------
 variantPath = "data/config/"
-variantFileName = "ship_variants.txt"
 variantTableName = "ship_variants.tbl"
-variantMatrix = {}
 variantTable = {}
 
 shipsToSet = {}
-
--- these variables need to be set through FRED's script-eval SEXP
-g_sn = nil -- global ship name
-g_vn = nil -- global variant name
 
 
 -----------------------
@@ -70,91 +85,19 @@ g_vn = nil -- global variant name
 
 
 
--- screw you, 32 char limit
-function sv(shipName, variantName)
-	setVariant(shipName, variantName)
-end
-
 
 --------------------
 -- core functions --
 --------------------
 
-function parseVariantFile(fileName, filePath)
-	if (matrix == nil) then
-		matrix = {}
-	end
-	if cf.fileExists(fileName, filePath, true) then
-		local file = cf.openFile(fileName, "r", filePath)
-		
-		local line = file:read("*l")
-		while (not (line == nil)) do
-			line = removeComments(line)
-			line = trim(line)
-			
-			cut = string.find(line, ":")
-			if not (cut == nil) then -- we've got a parsable line
-				if not (string.find(line, "[+]") == nil) then
-					if (attributeName == nil) then
-						ba.warning("parseVariantFile: assigning a sub-attribute without a super-attribute: "..line)
-					else
-						subAttributeName = trim(string.sub(line, 2, cut - 1))	-- armor, texture
-						subAttributeValue = trim(string.sub(line, cut + 1))		-- armor type, texture name
-						ba.print("parseVariantFile: subAttributeName="..subAttributeName.."; subAttributeValue="..subAttributeValue)
-						matrix[className][variantName]["+"..attributeName..":"..subAttributeName] = subAttributeValue
-					end
-				elseif not (string.find(line, "[$]") == nil) then
-					className = trim(string.sub(line, 2, cut - 1))
-					variantName = trim(string.sub(line, cut + 1))
-					
-					-- init class & variant maps
-					if (matrix[className] == nil) then
-						matrix[className] = {}
-					end
-					ba.print("parseVariantFile: className="..className.."; variantName="..variantName)
-					matrix[className][variantName] = {}
-					
-					-- clean up potential leftovers
-					attributeName = nil
-					attributeValue = nil
-				else
-					attributeName = trim(string.sub(line, 0, cut - 1))		-- turret, hull, armor, texture, team color
-					attributeValue = trim(string.sub(line, cut + 1))		-- weapon type, armor type, hull value, texture name, color name
-					
-					-- special case: texture attribute
-					if (attributeName == "texture") then
-						equal = string.find(line, "=")
-						attributeName = trim(string.sub(line, 0, equal - 1))	-- attributeName = texture:name
-						attributeValue = trim(string.sub(line, equal + 1))		-- attributeValue = replacement name
-					end
-					
-					ba.print("parseVariantFile: attributeName="..attributeName.."; attributeValue="..attributeValue)
-					matrix[className][variantName][attributeName] = attributeValue
-				end
-				--TODO: error check results of previous if then else block?
-			end
-			
-			line = file:read("*l")
-		end -- while
-		file:close()
-		return matrix
-	else
-		ba.warning("parseVariantFile: Variant file not found "..fileName)
-		return nil
-	end
-end
-
 function setVariant(shipName, variantName)
-	if (variantMatrix == nil) then
-		variantMatrix = parseVariantFile(variantFileName)
-	end
 	-- get ship class
 	-- lookup variant for that class
 	-- start going through the attributes
 	local ship = mn.Ships[shipName]
 	if not (ship:isValid()) then
-		ba.print("setVariant: Could not find ship "..shipName)
-		ba.print("setVariant: adding ship/variant to shipsToSet")
+		ba.print("[shipVariant.lua] Could not find ship "..shipName.."\n")
+		ba.print("[shipVariant.lua] adding ship/variant to shipsToSet\n")
 		shipsToSet[shipName] = variantName
 		return nil
 	end
@@ -162,24 +105,27 @@ function setVariant(shipName, variantName)
 	local className = ship.Class.Name
 	local variantInfo = variantMatrix[className][variantName]
 	if (variantInfo == nil) then
-		ba.warning("setVariant: Could not find variant info for "..className..":"..variantName)
+		ba.warning("[shipVariant.lua] Could not find variant info for "..className..":"..variantName.."\n")
 		return nil
 	end
 	
 	local turretArmor = ""
 	local subsystemArmor = ""
 	local subToSkip = {}
-	for attribute, value in pairs(variantInfo) do
-		--Note: value = variantInfo[attribute]
+	for attribute, prefix in pairs(variantInfo) do
+		local value = prefix['value']
+		
 		if (attribute == "armor") then
-			ba.print("setVariant: Armor ==> "..value)
+			ba.print("[shipVariant.lua] Armor ==> "..value.."\n")
 			ship.armorClass = value
 		elseif (attribute == "shield armor") then
-			ba.print("setVariant: Shield Armor ==> "..value)
+			ba.print("[shipVariant.lua] Shield Armor ==> "..value.."\n")
 			ship.ShieldArmorClass = value
 		elseif (attribute == "turret armor") then
+			ba.print("[shipVariant.lua] Global Turret Armor ==> "..value.."\n");
 			turretArmor = value
 		elseif (attribute == "subsystem armor") then
+			ba.print("[shipVariant.lua] Global Subsystem Armor ==> "..value.."\n");
 			subsystemArmor = value
 			
 		elseif (attribute == "hull") then
@@ -196,12 +142,13 @@ function setVariant(shipName, variantName)
 			end
 			
 			-- set max & current hit points
-			ba.print("setVariant: hull ==> "..value)
+			ba.print("[shipVariant.lua] Hull Max Hitpoints ==> "..value.."\n")
 			ratio = value / ship.HitpointsMax
 			ship.HitpointsMax = value
 			ship.HitpointsLeft = ship.HitpointsLeft * ratio
 			
 		elseif (attribute == "team color") then
+			ba.print("[shipVariant.lua] Team Color ==> "..value.."\n");
 			mn.evaluateSEXP([[
 				(when (true)
 					(change-team-color
@@ -213,6 +160,7 @@ function setVariant(shipName, variantName)
 			]])
 			
 		elseif (attribute == "ai class") then
+			ba.print("[shipVariant.lua] AI Class ==> "..value.."\n");
 			mn.evaluateSEXP([[[
 				(when (true)
 					(change-ai-class
@@ -223,55 +171,65 @@ function setVariant(shipName, variantName)
 			]])
 			
 		elseif not (string.find(attribute, "texture") == nil) then --note: this doesn't work
-			local textureName = extractRight(attribute)
-			local texture = gr.loadTexture(textureName)
-			if (texture:isValid()) then
-				ba.print("setVariant: Replacing texture '"..textureName.."' with texture '"..value.."'.")
-				ship.Textures[textureName] = texture
-			else
-				ba.warning("setVariant: Texture "..value.." is invalid.")
-			end
+			--local textureName = extractRight(attribute)
+			--local texture = gr.loadTexture(textureName)
+			--if (texture:isValid()) then
+			--	ba.print("[ShipVariant.lua] Replacing texture '"..textureName.."' with texture '"..value.."'.\n")
+			--	ship.Textures[textureName] = texture
+			--else
+			--	ba.warning("[ShipVariant.lua] Texture "..value.." is invalid.\n")
+			--end
 			
-		elseif not (string.find(attribute, "+") == nil) then -- sub-attribute
-			local line = attribute
-			attribute = extractLeft(line)
-			local subAttribute = extractRight(line)
-			ba.print("setVariant: Setting sub attribute for "..attribute)
-			ba.print("setVariant:     "..subAttribute.." ==> "..value)
-			
-			if (subAttribute == "armor") then
-				ship[attribute].ArmorClass = value
-				-- also, need to make sure general armor settings don't override this
-				subToSkip[attribute] = true
-				
-			elseif (subAttribute == "RoF") then
-				mn.evaluateSEXP([[
-					(when (true)
-						(turret-set-rate-of-fire
-							"]]..shipName..[["
-							]]..value..[[
-							"]]..attribute..[["
-						)
-					)
-				]])
-				
-			else
-				ba.warning("setVariant: Unrecognised sub attribute: "..subAttribute)
-			end
+		
 			
 		else -- turret
 			if (ship[attribute].PrimaryBanks) then -- primary banks
 				for i = 0, #ship[attribute].PrimaryBanks do
-					ba.print("setVariant: Prim "..attribute.." - "..ship[attribute].PrimaryBanks[i].WeaponClass.Name.." ==> "..tb.WeaponClasses[value].Name)
+					ba.print("[ShipVariant.lua] Primary Bank: "..attribute.." - "..ship[attribute].PrimaryBanks[i].WeaponClass.Name.." ==> "..tb.WeaponClasses[value].Name.."\n")
 					ship[attribute].PrimaryBanks[i].WeaponClass = tb.WeaponClasses[value]
 				end
 			else -- secondary banks
 				for i = 0, #ship[attribute].SecondaryBanks do
-					ba.print("setVariant: Sec "..attribute.." - "..ship[attribute].SecondaryBanks[i].WeaponClass.Name.." ==> "..tb.WeaponClasses[value].Name)
+					ba.print("[ShipVariant.lua] Secondary Bank: "..attribute.." - "..ship[attribute].SecondaryBanks[i].WeaponClass.Name.." ==> "..tb.WeaponClasses[value].Name.."\n")
 					ship[attribute].SecondaryBanks[i].WeaponClass = tb.WeaponClasses[value]
 				end
 			end
 		end
+		
+		 -- sub-attributes
+		if not (prefix['sub'] == nil) then
+			for subAttribute, subValue in pairs(prefix['sub']) do
+				ba.print("[shipVariant.lua]     "..subAttribute.." ==> "..subValue)
+				
+				if (subAttribute == "armor") then
+					ship[attribute].ArmorClass = subValue
+					-- also, need to make sure general armor settings don't override this
+					subToSkip[attribute] = true
+					
+				elseif (subAttribute == "RoF") then
+					mn.evaluateSEXP([[
+						(when (true)
+							(turret-set-rate-of-fire
+								"]]..shipName..[["
+								]]..subValue..[[
+								"]]..attribute..[["
+							)
+						)
+					]])
+					
+				else
+					ba.warning("[shipVariant.lua] Unrecognised sub attribute: "..subAttribute)
+				end
+			end
+		end
+		
+	end
+	ba.print("[shipVariant.lua] all attributes set, moving on to global settings...\n");
+	if not (turretArmor == "") then
+		ba.print("[shipVariant.lua] Turret Armor ==> "..turretArmor.."\n")
+	end
+	if not (subsystemArmor == "") then
+		ba.print("[shipVariant.lua] Subsystem Armor ==> "..subsystemArmor.."\n")
 	end
 	
 	-- set turrets & subsystems armor
@@ -299,23 +257,8 @@ function setVariantDelayed()
 end
 
 
---[[
-	set variant using global variables g_sn & g_vn
-]]--
-function setVariant_g()
-	if (g_sn == nil) then
-		ba.warning("setVariant_g: global variable g_sn unset.")
-	elseif (g_vn == nil) then
-		ba.warning("setVariant_g: global variable g_vn unset.")
-	else
-		setVariant(g_sn, g_vn)
-		g_sn = nil
-		g_vn = nil
-	end
-end
 ----------
 -- main --
 ----------
 
---variantMatrix = parseVariantFile(variantFileName, variantPath)
 variantTable = parseTableFile(variantPath, variantTableName)
