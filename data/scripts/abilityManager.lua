85a7ccbba7c18f0c1894844b06a92bfa0de9fdad
Adrien <adrarsis@laposte.net>
2016-06-13 17:23:18 +0200

    Create abilityManager.lua

---
+++ data/scripts/abilityManager.lua
@@ -0,0 +1,10 @@
+
+function ability_createClass(classTable)
+  ability_classes[TODO] = {
+	  Name = TODO,
+	  Cooldown = TODO
+	}
+end
+
+abilityTable = parseTableFile("data/config/", "abilities.tbl")
+
