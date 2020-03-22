1f17755a1b5ebefcdd1600a529fb3ef3825dbd03
X3N0-Life-Form <adrarsis@laposte.net>
2020-03-22 14:57:39 +0100

    started work on waypoint highlight script

--- core/data/scripts/waypointHighlight.lua
+++ core/data/scripts/waypointHighlight.lua
@@ -14,6 +14,17 @@
 
 waypointHighlight_colors = {}
 
+waypointHighlight_enableDebugPrint = true
+
+-----------------------
+-- utility functions --
+-----------------------
+
+function dPrint_waypointHighlight(message)
+	if (waypointHighlight_enableDebugPrint) then
+		ba.print("[waypointHighlight.lua] "..message.."\n")
+	end
+end
 
 ----------------------
 --- Core functions ---
@@ -24,45 +35,105 @@ function waypointHighlight_init()
 	-- Reset global vars
 	waypointHighlight_colors = {}
 	
-	for i = 0, #mn.WaypointLists do
+	dPrint_waypointHighlight("Initializing highlight colors...")
+	for i = 1, #mn.WaypointLists do
 		local wpList = mn.WaypointLists[i]
 		-- Set waypoint path color
 		local red = math.random(255)
 		local green = math.random(255)
 		local blue = math.random(255)
-		waypointHighlight_colors[wpList.Name] = [red, green , blue]
+		waypointHighlight_colors[wpList.Name] = {red, green , blue}
+		dPrint_waypointHighlight("\t"..wpList.Name.." : {red="..red.."; green="..green.."; blue="..blue.."}")
 	end
 end
 
 --- Highlight mission waypoints
 --- Must be called per frame
 function waypointHighlight_highlight()
-	for i = 0, #mn.WaypointLists do
+	dPrint_waypointHighlight("Beginning waypoint highlight...")
+	for i = 1, #mn.WaypointLists do
 		local wpList = mn.WaypointLists[i]
 		local color = waypointHighlight_colors[wpList.Name]
-		gr.setColor(color[0], color[1], color[2])
+		dPrint_waypointHighlight("Highlighting "..wpList.Name)
 		
-		for j = 0, #wpList do
+		gr.setColor(color[1], color[2], color[3])
+		
+		for j = 1, #wpList do
 			local waypoint = wpList[j]
 			local position = waypoint.Position
+			-- local coordinates = position:getScreenCoords()
+			local coordinates = getScreenCoordsActuallyWorks(position)
 			
-			-- draw sphere & brackets
-			local coordinates = gr.drawTargetingBrackets(waypoint, true)
-			gr.drawSphere(5.0, waypoint.Position)
-			
-			-- draw name
-			gr.drawString(wpList.Name..":"..j, coordinates[3], coordinates[1])
-			
-			-- draw coordinates
-			gr.drawString("x:"..position[1].." y:"..position[2].." z:"..position[3])
-			
-			-- draw distance to target
-			local playerShip = mn.Ships[0]
-			local target = playerShip.Target
-			local distance = target.Position.getDistance(position)
-			gr.drawString("distance to "..target.Name.." = "..distance)
+			if (coordinates) then
+				dPrint_waypointHighlight("\tPoint "..j.." ("..coordinates[1]..", "..coordinates[2]..")")
+				-- draw sphere & brackets
+				-- lol doesn't work on waypoints... (cf.graphics.cpp)
+				-- local coordinates = gr.drawTargetingBrackets(waypoint, true)
+				
+				gr.drawSphere(5.0, waypoint.Position)
+					
+				-- draw name
+				gr.drawString(wpList.Name..":"..j, coordinates[1], coordinates[2]+15)
+				
+				-- draw coordinates
+				gr.drawString("x:"..position[1])
+				gr.drawString("y:"..position[2])
+				gr.drawString("z:"..position[3])
+				
+				-- draw distance to waypoint
+				local playerShip = mn.Ships[0]
+				local distance = playerShip.Position:getDistance(position)
+				-- gr.drawString("distance = "..distance)
+			else
+				dPrint_waypointHighlight("\tPoint "..j.." is offscreen")
+			end
 		end
 		
 	end
 
+end
+
+-- cf.g3_rotate_vertex() & g3_project_vertex() in 3dmath.cpp
+function getScreenCoordsActuallyWorks(targetPosition)
+	dPrint_waypointHighlight("getScreenCoordsActuallyWorks begins")
+	-- rotation
+	local camera = gr.getCurrentCamera(true)
+	-- local tx = targetPosition.x - camera.Position.x
+	-- local ty = targetPosition.y - camera.Position.y
+	-- local tz = targetPosition.z - camera.Position.z
+	-- dPrint_waypointHighlight("\tTarget position: "..targetPosition.x..","..targetPosition.y..","..targetPosition.z)
+	-- dPrint_waypointHighlight("\tCamera position: "..camera.Position.x..","..camera.Position.y..","..camera.Position.z)
+	-- dPrint_waypointHighlight("\tt vars: "..tx..","..ty..","..tz)
+
+	-- rx = tx * camera.Orientation:getRvec().x
+	-- rx = rx + ty * camera.Orientation:getRvec().y
+	-- rx = rx + tz * camera.Orientation:getRvec().z
+
+	-- ry = tx * camera.Orientation:getUvec().x
+	-- ry = ry + ty * camera.Orientation:getUvec().y
+	-- ry = ry + tz * camera.Orientation:getUvec().z
+
+	-- rz = tx * camera.Orientation:getFvec().x;
+	-- rz = rz + ty * camera.Orientation:getFvec().y
+	-- rz = rz + tz * camera.Orientation:getFvec().z
+	-- dPrint_waypointHighlight("\tr vars: "..rx..","..ry..","..rz)
+	
+	--
+	local rvec = camera.Orientation:rotateVector(targetPosition)
+	rx = rvec.x
+	ry = rvec.y
+	rz = rvec.z
+	
+	-- projection
+	local width = gr.getScreenWidth()
+	local height = gr.getScreenHeight()
+	local w = 1.0 / rz
+	dPrint_waypointHighlight("\t"..width.."x"..height)
+	dPrint_waypointHighlight("\tw="..w)
+	
+	local x = (width + (rx * width * w)) * 0.5
+	-- local y = (height - (ry * height * w)) * 0.5
+	local y = (height - (ry * height * w)) * 0.5
+	dPrint_waypointHighlight("\tx="..x..", y="..y)
+	return {x, y}
 end
